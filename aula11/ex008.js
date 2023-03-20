99160ebc7398d9beaaaec65a36df7329c6cd6b2d
Edilson <semchefe41@gmail.com>
2023-03-20 10:28:46 -0300

    Initial commit

---
+++ aula11/ex008.js
@@ -0,0 +1,5 @@
+var vel = 78.2
+if (vel > 60) {
+    console.log('Voce ultrapassou a velocidade permitida. MULTADO!')
+}
+console.log('Dirija sempre usando cinto de segurança!')
---
+++ aula12ex/modelo/script.js
@@ -0,0 +1,1 @@
+window.alert('Ola tudo bem')
